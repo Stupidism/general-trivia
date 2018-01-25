be729748255f362c485488083ab920ca14b4dafe
stupid <sunfeng32@qq.com>
2018-01-25 18:42:26 +0800

    chore(package): support antd

---
+++ config-overrides.js
@@ -0,0 +1,23 @@
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
+module.exports = {
+  webpack: overrideWebpack,
+  jest: overrideJest,
+};
+
+function overrideWebpack(config, env) {
+  config = injectBabelPlugin(['import', [
+    { libraryName: 'antd', style: true },
+    { libraryName: 'antd-mobile', style: true },
+  ]], config);
+
+  config = rewireLess.withLoaderOptions({})(config, env);
+
+  return config;
+}
+
+function overrideJest(config) {
+  config.setupTestFrameworkScriptFile = '<rootDir>/jest-setup.js';
+  return config;
+}
