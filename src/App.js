74c01a9c69a993f7c600544f17b91ec742ef4e17
stupid <sunfeng32@qq.com>
2018-01-25 18:46:39 +0800

    feat(App): display all info and handle process

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Webcam from 'react-webcam';
+import { Progress } from 'antd';
+
+import Question from './components/Question';
+
+import search from './utils/search';
+import recognize from './utils/recognize';
+
+import exampleImg from './assets/example.jpg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    question: null,
+    pages: null,
+    progress: null,
+  };
+
+  componentDidMount() {
+  }
+
+  setRef = (webcam) => {
+    this.webcam = webcam;
+  };
+
+  handleProgressChange = (progress) => this.setState({ ...progress });
+
+  start = async (url) => {
+    this.startAt = Date.now();
+    const image = url ? url : this.webcam.getScreenshot();
+    this.setState({ image });
+    const question = await recognize(image, this.handleProgressChange);
+    this.setState({ question });
+    const pages = await search(question);
+    this.setState({ pages, timeCost: (Date.now() - this.startAt) / 1000 });
+  };
+
   render() {
+    const { image, question, pages, progress, status, timeCost } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <button onClick={this.start}>开始</button>
+          <button onClick={() => this.start(exampleImg)}>测试</button>
+        </div>
+        <Webcam
+          audio={false}
+          height={350}
+          ref={this.setRef}
+          screenshotFormat="image/jpeg"
+          width={350}
+        />
+        {image ? <img width={300} src={image} alt="Image"/> : null }
+        {status ? (
+          <div>
+            {status}
+            <Progress percent={progress * 100} />
+          </div>
+        ): null}
+        {question ? <Question key={question.statement} {...question} pages={pages} /> : null}
+        {timeCost ? `花了${timeCost}秒钟` : null}
       </div>
     );
   }
